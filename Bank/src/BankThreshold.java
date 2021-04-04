2998c46a7b55e45d817caf9d4b25bf9cf85500ae
Joe <Joe@Joe.myfiosgateway.com>
2021-04-04 18:42:13 -0400

    This is the third version of my bank account project

--- Bank/src/BankThreshold.java
+++ Bank/src/BankThreshold.java
@@ -36,7 +36,7 @@ public class BankThreshold {
 				months++;
 			}
 			
-			myBankAccount.restoreOpeningBalance(startBal);
+			myBankAccount.restoreOpeningBalance(startBal); //
 			
 			System.out.println("	It will take " + months + " months to reach the threshold of " + threshold + ".");
 			System.out.print("Enter the threshold for your account balance to reach or 'End': ");
